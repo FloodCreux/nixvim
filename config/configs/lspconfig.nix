9453ebecedceb3cd6da129c97f5c8c15dd3add55
Mike Flood <flood.mike@gmail.com>
2024-01-16 12:15:39 -0600

    feat: Add key mappings and settings for Metals and LSP
    This commit adds key mappings and settings for the Metals language server protocol (LSP) in the `lspconfig.nix` file. The new mappings include commands for toggling tree view, creating new Scala files, revealing items in the tree view, and showing implicit arguments and semantic highlights. Additionally, code lens running and code actions are mapped to specific keys.

--- config/configs/lspconfig.nix
+++ config/configs/lspconfig.nix
@@ -30,6 +30,35 @@
               mavenScript = "mvn ch.epfl.scala:bloop-maven-plugin:2.0.0:bloopInstall";
           };
         };
+
+        onAttach = {
+          function = ''
+          local function mapB(mode, l, r, desc)
+            local opts = { noremap = true, silent = true, buffer = bufnr, desc = desc }
+            map(mode, l, r, opts)
+          end
+
+          metals.setup_dap()
+          map("n", "<leader>mc", require("telescope").extensions.metals.commands, { desc = "Metals commands" })
+
+          mapB("v", "R", metals.type_of_range, "metals: type of range")
+
+          mapB("n", "<leader>mt", require("metals.tvp").toggle_tree_view, "Metals toggle tree view")
+          mapB("n", "<leader>mf", "<cmd>MetalsNewScalaFile<cr>", "Metals create new scala file")
+          mapB("n", "<leader>mr", require("metals.tvp").reveal_in_tree, "Metals reveal in tree")
+          mapB("n", "<leader>msi", function()
+                  require("metals").toggle_setting("showImplicitArguments")
+          end, "Metals show implicit arguments")
+          mapB("n", "<leader>mss", function()
+                  require("metals").toggle_setting("enableSemanticHighlighting")
+          end, "Metals show semantic highlights")
+
+          mapB("n", "<leader>lc", vim.lsp.codelens.run, "LSP run code lens")
+          mapB("n", "<leader>la", vim.lsp.buf.code_action, "LSP code action")
+          '';
+
+          override = true;
+        };
       };
       # csharp-ls = {
       #   enable = true;
